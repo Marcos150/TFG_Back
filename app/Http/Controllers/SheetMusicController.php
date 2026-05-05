d18befb23893340a12d8cb89d6218b4a7cf4d644
Marcos150 <marcosruizrubio@gmail.com>
2026-05-05 22:24:26 +0200

    Ordenación de compases

--- app/Http/Controllers/SheetMusicController.php
+++ app/Http/Controllers/SheetMusicController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\SheetMusic;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -24,10 +25,21 @@ class SheetMusicController extends Controller
     public function store(Request $request)
     {
         $path = $request->file('file')->store('files');
+        $measures = json_decode($request->input('measures'), true);
+        $sortedMeasures = collect($measures)->sort(function ($a, $b) {
+            $tolerance = 0.05;
+
+            if (abs($a['top'] - $b['top']) > $tolerance) {
+                return $a['top'] <=> $b['top'];
+            }
+
+            return $a['left'] <=> $b['left'];
+        })->values()->all();
         $sheetMusic = SheetMusic::create([
             'title' => $request->input('title'),
             'author' => $request->input('author'),
             'file_path' => $path,
+            'measures' => $sortedMeasures,
         ]);
 
         if ($request->has('tags')) {
@@ -45,7 +57,7 @@ class SheetMusicController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id): \Illuminate\Http\JsonResponse
+    public function show(string $id): JsonResponse
     {
         $sheetMusic = SheetMusic::find($id)->load('tags');
 
@@ -58,7 +70,17 @@ class SheetMusicController extends Controller
     public function update(Request $request, string $id)
     {
         $sheetMusic = SheetMusic::find($id);
-        $sheetMusic->update($request->all());
+        $measures = $request->input('measures');
+        $sortedMeasures = collect($measures)->sort(function ($a, $b) {
+            $tolerance = 0.05;
+
+            if (abs($a['top'] - $b['top']) > $tolerance) {
+                return $a['top'] <=> $b['top'];
+            }
+
+            return $a['left'] <=> $b['left'];
+        })->values()->all();
+        $sheetMusic->update(array_merge($request->all(), ['measures' => $sortedMeasures]));
 
         return response()->json($sheetMusic);
     }
@@ -80,6 +102,7 @@ class SheetMusicController extends Controller
         $file = Storage::get($sheetMusic->file_path);
 
         $contentType = Storage::mimeType($sheetMusic->file_path);
+
         return response($file, 200)->header('Content-Type', $contentType);
     }
 }
